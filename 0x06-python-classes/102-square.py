b24ba878e5ff26c90f61e2da175efacbcfa6e6f5
Meriame Melkaoui <meriamemelkaoui2003@gmail.com>
2023-06-27 17:31:42 -0700

    commit

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,20 +1,9 @@
-
-#!/usr/bin/python3
-"""Module containing the Square class"""
-
-
 class Square:
-    """The Square class"""
     def __init__(self, size=0):
-        """Initializing an instance of Square
-        Args:
-            size (int): The size of the Square instance. Default value is 0.
-        """
         self.size = size
 
     @property
     def size(self):
-        """int: size of the Square instance"""
         return self.__size
 
     @size.setter
@@ -27,95 +16,35 @@ class Square:
         self.__size = size
 
     def area(self):
-        """Returns the current square area of the instance
-        Returns:
-            int: The square of size
-        """
         return self.__size ** 2
 
     def __eq__(self, other):
-        """Check if the area of the instance is the same as the area of 'other'.
-        Returns:
-            bool: The return Value. True if self.area == other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() == other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() == other.area()
+        return False
 
     def __ne__(self, other):
-        """Check if the area of the instance is not the same as the area of
-            'other'.
-        Returns:
-            bool: The return Value. True if self.area != other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() != other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() != other.area()
+        return False
 
     def __gt__(self, other):
-        """Check if the area of the instance is greater than the area of 'other'.
-        Returns:
-            bool: The return Value. True if self.area > other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() > other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() > other.area()
+        return False
 
     def __ge__(self, other):
-        """Check if the area of the instance is greater than or equal to the
-            area of 'other'.
-        Returns:
-            bool: The return Value. True if self.area >= other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() >= other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() >= other.area()
+        return False
 
     def __lt__(self, other):
-        """Check if the area of the instance is less than the area of 'other'.
-        Returns:
-            bool: The return Value. True if self.area < other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() < other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() < other.area()
+        return False
 
     def __le__(self, other):
-        """Check if the area of the instance is less than or equal to the area
-            of 'other'.
-        Returns:
-            bool: The return Value. True if self.area <= other.area. False
-                otherwise, or 'other' is not of type Square.
-        """
         if isinstance(other, Square):
-            if self.area() <= other.area():
-                return True
-            else:
-                return False
-        else:
-            return False
+            return self.area() <= other.area()
+        return False
+
