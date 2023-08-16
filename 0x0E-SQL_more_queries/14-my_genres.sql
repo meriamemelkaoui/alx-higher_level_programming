d564de3d13c8557f1e0e740dc5ab66a73e07a7ce
Meriame Melkaoui <meriamemelkaoui2003@gmail.com>
2023-08-16 08:25:05 -0700

    commit

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- List all genres for Dexter
+SELECT g.name AS name
+FROM tv_shows s INNER JOIN tv_show_genres sg
+  ON s.id = sg.show_id
+INNER JOIN tv_genres g
+  ON sg.genre_id = g.id
+WHERE s.title = 'Dexter'
+ORDER BY g.name;
