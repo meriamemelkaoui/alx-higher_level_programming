aae77f9e5e63af7815c5ad92b745052da20e23f8
Meriame Melkaoui <meriamemelkaoui2003@gmail.com>
2023-08-15 17:33:03 -0700

    commit

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Change the database: `hbtn_0c_0`i to UTF8
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change database
+USE hbtn_0c_0;
+-- Change all and future columns in `first_table` to UTF8
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
